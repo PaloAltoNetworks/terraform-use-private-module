7f387bdfde60895677fa905595c5e80ce39a6ffd
Simon Melotte <smelotte@paloaltonetworks.com>
2022-07-13 15:40:35 +0200

    add outputs.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 module "site-deploy" {
-  source = "github.com/SimOnPanw/terraform-private-module.git//site-deploy"
+  source = "github.com/SimOnPanw/terraform-private-module//static-website/site-deploy?ref=main"
+
+  bucket_name = "smelotte-depoy-statuc-website"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }

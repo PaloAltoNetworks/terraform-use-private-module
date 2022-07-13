b71b726c89c1c0658149f5932cccacacde019150
Simon Melotte <91271604+SimOnPanw@users.noreply.github.com>
2022-07-13 23:06:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "site-deploy" {
-  source = "github.com/SimOnPanw/terraform-private-module//static-website/site-deploy?ref=main"
+  source = "github.com/SimOnPanw/terraform-private-module//static-website"
 
   bucket_name = "smelotte-depoy-statuc-website"
 

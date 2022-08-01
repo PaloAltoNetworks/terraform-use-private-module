9477b6099781505b1d43b4e8858e13f0e6d86339
Simon Melotte <91271604+SimOnPanw@users.noreply.github.com>
2022-08-02 01:13:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "static-website" {
   #source = "git::https://github.com/SimOnPanw/terraform-private-module.git"
-  source = "git::git@github.com:gruntwork-io/terraform-google-gke.git//modules/gke-cluster?ref=v0.1.2"
+  source = "git::https://github.com/gruntwork-io/terraform-google-gke.git//modules/gke-cluster?ref=v0.1.2"
   bucket_name = "smelotte-depoy-static-website"
 
   tags = {

bf02b8b8801b2bf1f9346cd229bdbe457f6737ac
Simon Melotte <91271604+SimOnPanw@users.noreply.github.com>
2022-08-02 01:46:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "google" {
+  project     = "my-project-id"
+  region      = "us-central1"
+} 
+
 module "static-website" {
   #source = "git::https://github.com/SimOnPanw/terraform-private-module.git"
   source = "git::https://github.com/gruntwork-io/terraform-google-gke.git//modules/gke-cluster?ref=v0.1.2"
